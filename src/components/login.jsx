b561f0943f66efda0d9a44f5cc07b499afadcde8
Harry <harshyadav3577@gmail.com>
2023-08-10 14:51:37 +0530

    created login handler to Redirect next page

---
+++ src/components/login.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function Login({ onLogin }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+
+    if (username === 'user' && password === 'password') {
+      onLogin(username);
+    } else {
+      alert('Invalid credentials');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <div>
+        <label>Username:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
